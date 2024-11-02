2ca765a0bbca6da4d89afd22ad9553656b4b98f1
MGergont <mateuszgergont06@gmail.com>
2024-11-02 11:03:06 +0100

    style(user): adding a leyaut for operator accounts

--- Src/Views/Template/operator/user/dashboardUser.php
+++ Src/Views/Template/operator/user/dashboardUser.php
@@ -1,20 +1,72 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Apliakcja internetowa ułatwiająca pracownikom oraz pracodawcy zarzadzane wyjazdami służbowymi">
-    <meta name="keywords" content="travel, management, business, tasks, coordination, planning, automation, trips">
-
-    <title>TravelFlow.pl</title>
-
-    <link rel="icon" href="/Public/image/icon.png" type="image/png">
-    <link rel="stylesheet" href="/Public/css/main.min.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Apliakcja internetowa ułatwiająca pracownikom oraz pracodawcy zarzadzane wyjazdami służbowymi">
+	<meta name="keywords" content="travel, management, business, tasks, coordination, planning, automation, trips">
+	<title>TravelFlow.pl</title>
+	<link rel="icon" href="/Public/image/icon.png" type="image/png">
+	<link rel="stylesheet" href="/Public/css/main.min.css">
+	<link rel="stylesheet" href="/Public/fonts/icon/fontello/css/fontello.css">
 </head>
-    <body>
-        <h2>User Dashboard</h2>
+<body>
+	<header class="topbar">
+		<div class="topbar__hamburger">
+			<i class="topbar__bar icon-menu"></i>
+		</div>
+		<img class="topbar__logo" src="/Public/image/logo_main.png" alt="logo Travel Flow" />
+		<img class="topbar__logo2" src="/Public/image/logo_slim.png" alt="logo Travel Flow" />
+		<div class="menu-user">
+			<ul class="menu-user__list">
+				<li class="menu-user__item menu-user__item--dropdown">
+					<a href="#" class="menu-user__link1"><?php echo $_SESSION['userLogin']; ?><i class="icon-down-open"></i></a>
+					<a href="#" class="menu-user__link2"><i class="icon-torso"></i></a>
+					<ul class="menu-user__dropdown">
+						<li class="menu-user__dropdown-item">
+							<a href="#" class="menu-user__dropdown-link">Profil</a>
+						</li>
+						<li class="menu-user__dropdown-item">
+							<a href="#" class="menu-user__dropdown-link">Ustawienia</a>
+						</li>
+						<li class="menu-user__dropdown-item">
+							<a href="/logout" class="menu-user__dropdown-link">wyloguj się</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</header>
+	<div>
+		<div class="sidebar">
+			<nav class="sidebar__menu">
+				<ul class="sidebar__list">
+					<li class="sidebar__item"><a href="/user-dashboard" class="sidebar__link">Strona główna</a></li>
+					<li class="sidebar__item"><a href="#" class="sidebar__link">Services</a></li>
+					<li class="sidebar__item"><a href="#" class="sidebar__link">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+		<main class="content">
+			<h2 class="content__title">User Dashboard</h2>
+			<div class="user-panel">
 
-        <a href="/logout-ope">wyloguj się</a>
+			</div>
+		</main>
+	</div>
+</body>
+<script>
+	const sidebar = document.querySelector('.sidebar');
+	const topbarHamburger = document.querySelector('.topbar__hamburger');
 
-    </body>
+	function toggleSidebar() {
+		sidebar.classList.toggle('sidebar--hidden');
+	}
+
+	if (topbarHamburger) {
+		topbarHamburger.addEventListener('click', toggleSidebar);
+	}
+</script>
+
 </html>
