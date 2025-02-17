0628457a8b2757de63da0165fb7b95e793a6852e
MGergont <mateuszgergont06@gmail.com>
2025-02-17 20:32:57 +0100

    feat(dashboard): adding a dashboard for the manager

--- Src/Views/Template/operator/manager/dashboardManager.php
+++ Src/Views/Template/operator/manager/dashboardManager.php
@@ -11,6 +11,7 @@
 	<link rel="stylesheet" href="/Public/css/main.min.css">
 	<link rel="stylesheet" href="/Public/fonts/icon/fontello/css/fontello.css">
 </head>
+
 <body>
 	<header class="topbar">
 		<div class="topbar__hamburger">
@@ -44,7 +45,7 @@
 				<ul class="sidebar__list">
 					<li class="sidebar__item"><a href="/manager-dashboard" class="sidebar__link">Strona główna</a></li>
 					<li class="sidebar__item"><a href="/manager/order" class="sidebar__link">Zlecenia Delegacji</a></li>
-                    <li class="sidebar__item"><a href="/manager/route" class="sidebar__link">Delegacja</a></li>
+					<li class="sidebar__item"><a href="/manager/route" class="sidebar__link">Delegacja</a></li>
 					<li class="sidebar__item"><a href="/manager/location" class="sidebar__link">Lokalizacje</a></li>
 					<li class="sidebar__item"><a href="/manager/fleet" class="sidebar__link">Flota</a></li>
 					<li class="sidebar__item"><a href="/manager/vehicle" class="sidebar__link">Samochód</a></li>
@@ -54,8 +55,45 @@
 		</div>
 		<main class="content">
 			<h2 class="content__title">Manager Dashboard</h2>
-			<div class="user-panel">
-
+			<div class="dashboard-manager">
+				<?php if (isset($params['StatusOrders'])): ?>
+					<div class="dashboard-manager__section">
+						<h2 class="dashboard-manager__title">Status realizacji zamówień</h2>
+						<div class="dashboard-manager__routes">
+							<?php foreach ($params['StatusOrders'] as $orders): ?>
+								<div class="dashboard-manager__route">
+									<span><strong><?php echo $orders["status_order"] . " " . $orders["order_count"] . "szt. " ?></strong></span>
+								</div>
+							<?php endforeach ?>
+						</div>
+					</div>
+				<?php endif; ?>
+				<?php if (isset($params['ActiveCar'])): ?>
+					<div class="dashboard-manager__section">
+						<h2 class="dashboard-manager__title">Samochody z największymi kosztami eksploatacji (ostatnie 6 miesięcy)</h2>
+						<?php foreach ($params['carCost'] as $cost): ?>
+							<p class="dashboard-manager__costs"><?php echo $cost['license_plate'] . "<br> " . $cost['brand'] . " " . $cost['model'] . " koszt: " . $cost['total_expenses']; ?> PLN</p>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+				<?php if (isset($params['ActiveCar'])): ?>
+					<div class="dashboard-manager__section">
+						<h2 class="dashboard-manager__title">Liczba aktywnych samochodów</h2>
+						<p class="dashboard-manager__costs"><?php echo $params['ActiveCar']['active_cars']; ?> szt</p>
+					</div>
+				<?php endif; ?>
+				<?php if (isset($params['EndInsurance'])): ?>
+					<div class="dashboard-manager__section">
+						<h2 class="dashboard-manager__title">Status realizacji zamówień</h2>
+						<div class="dashboard-manager__routes">
+							<?php foreach ($params['EndInsurance'] as $car): ?>
+								<div class="dashboard-manager__route">
+									<span><strong><?php echo $car["id_car"] . " | " . $car["license_plate"] . " | " . $car["brand"] . " | " . $car["model"] . " | " . date('Y-m-d', strtotime($car["end_of_insurance"])) ?></strong></span>
+								</div>
+							<?php endforeach ?>
+						</div>
+					</div>
+				<?php endif; ?>
 			</div>
 		</main>
 	</div>
